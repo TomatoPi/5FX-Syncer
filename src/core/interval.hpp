a56118cb102fe5fc73e2a6325d0bb1df704c6565
Dago Esaie <dago.esaie@protonmail.com>
2022-08-11 16:20:14 +0200

    chg: changed interval functions to functor objects

--- src/core/interval.hpp
+++ src/core/interval.hpp
@@ -11,30 +11,31 @@ namespace sfx {
             T begin;    /**< Included lower bound */
             T end;      /**< Excluded upper bound */
 
-            explicit constexpr operator bool () const { return begin < end; }
+            explicit constexpr operator bool () const noexcept
+                { return begin < end; }
         };
 
         template <typename T>
-        constexpr interval<T> operator>> (interval<T> i, T t) 
-            { return interval<T>{i.begin + t, i.end + t}; }
-        template <typename T>
-        constexpr interval<T> operator<< (interval<T> i, T t) 
-            { return interval<T>{i.begin - t, i.end - t}; }
-        
-        template <typename T, typename F>
-        constexpr interval<F> convert (interval<T> i, F f) 
-            { return interval<F>{static_cast<F>(i.begin * f), static_cast<F>(i.end * f)}; }
-            
-        template <typename T>
-        constexpr bool contains(interval<T> i, T t) { return i.begin <= t && t < i.end; }
-        template <typename T, typename ...Ts>
-        constexpr bool contains(interval<T> i, T t, Ts... ts) { return contains(i, t) && contains(i, ts...); }
+        struct shift
+        {
+            T diff;
+            constexpr interval<T> operator() (interval<T> i) const noexcept
+                { return interval<T>{i.begin + diff, i.end + diff}; }
+        };
 
         template <typename T>
-        constexpr interval<T> intersection (interval<T> a, interval<T> b)
-            { return interval<T>{std::max(a.begin, b.begin), std::min(a.end, b.end)}; }
-        template <typename T, typename ...Ts>
-        constexpr interval<T> intersection (interval<T> a, interval<T> b, interval<Ts>... v)
-            { return intersection(intersection(a, b), v...); }
+        struct contains
+        {
+            T val;
+            constexpr bool operator() (interval<T> i) const noexcept
+                { return i.begin <= val && val < i.end; }
+        };
+
+        template <typename T>
+        struct intersect
+        {
+            constexpr interval<T> operator() (interval<T> a, interval<T> b) const noexcept
+                { return interval<T>{std::max(a.begin, b.begin), std::min(a.end, b.end)}; }
+        };
     }
 }
