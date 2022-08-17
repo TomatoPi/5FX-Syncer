a47cda24eb25dc2ef73e056f1040a5b8363a2316
Dago Esaie <dago.esaie@protonmail.com>
2022-08-17 11:11:41 +0200

    new: added string literals for sr and bpm new: more obvious tests

--- src/core/time.hpp
+++ src/core/time.hpp
@@ -11,12 +11,20 @@ namespace sfx {
             /** 1BPM == 24 ticks per 60 seconds (Hardcoded 24PPQN) */
             using base = std::ratio<24, 60>;
         };
+        constexpr bpm operator"" _bpm(long double b)
+            { return bpm{static_cast<bpm::type>(b)}; }
+        constexpr bpm operator"" _bpm(unsigned long long int b)
+            { return bpm{static_cast<bpm::type>(b)}; }
         
         struct frame        : strong_type<uint32_t, frame> {};
         struct samplerate   : strong_type<uint32_t, samplerate> {
             /**< 1Hz == 1 frame per 1 second */
             using base = std::ratio<1,1>;
         };
+        constexpr samplerate operator"" _Hz(unsigned long long int sr)
+            { return samplerate{static_cast<samplerate::type>(sr)}; }
+        constexpr samplerate operator"" _kHz(unsigned long long int sr)
+            { return samplerate{static_cast<samplerate::type>(sr) * 1000}; }
 
         template <typename Repr, typename Rate>
         struct timestamp {
@@ -29,7 +37,7 @@ namespace sfx {
         using ticktime = timestamp<tick, bpm>;
         using frametime = timestamp<frame, samplerate>;
         
-        template <typename From, typename FromBase, typename To, typename ToBase>
+        template <typename To, typename ToBase, typename From, typename FromBase>
         constexpr timestamp<To, ToBase> convert(timestamp<From, FromBase> t, ToBase tr)
         {
             using factor = std::ratio<FromBase::base::den * ToBase::base::num, FromBase::base::num * ToBase::base::den>;
@@ -37,7 +45,7 @@ namespace sfx {
             return timestamp<To, ToBase>{value, tr};
         }
 
-        constexpr auto tick_to_frame = convert<tick, bpm, frame, samplerate>;
-        constexpr auto frame_to_tick = convert<frame, samplerate, tick, bpm>;
+        constexpr auto frame_to_tick = convert<tick, bpm, frame, samplerate>;
+        constexpr auto tick_to_frame = convert<frame, samplerate, tick, bpm>;
     }
 }
--- tests/time.cpp
+++ tests/time.cpp
@@ -7,16 +7,25 @@ int main(int argc, char * const argv[])
 {
     using namespace sfx::time;
 
-    constexpr samplerate _48khz{48000};
+    constexpr samplerate _48kHz{48000};
+    constexpr samplerate _92kHz{92000};
     constexpr bpm _60bpm{60};
     constexpr bpm _120bpm{120};
 
-    constexpr frametime one_sec_f{48000, _48khz};  /* 48k samples at 48kHz == 1s */
+    constexpr frametime one_sec_f{48000, _48kHz};  /* 48k samples at 48kHz == 1s */
     constexpr ticktime one_sec_t_60{24, _60bpm};   /* 24 ticks == 1beat == 1s at 60bpm */
     constexpr ticktime one_sec_t_120{48, _120bpm}; /* 48 ticks == 2beat == 1s at 120bpm */
 
-    static_assert(tick_to_frame(one_sec_t_60, _48khz).value == one_sec_f.value);
-    static_assert(tick_to_frame(one_sec_t_120, _48khz).value == one_sec_f.value);
+    static_assert(_48kHz == 48_kHz);
+    static_assert(_48kHz == 48000_Hz);
+
+    static_assert(_60bpm == 60.0_bpm);
+    static_assert(_60bpm == 60_bpm);
+
+    static_assert(tick_to_frame(one_sec_t_60, _48kHz).value == one_sec_f.value);
+    static_assert(tick_to_frame(one_sec_t_120, _48kHz).value == one_sec_f.value);
+    static_assert(frame_to_tick(one_sec_f, _60bpm).value == one_sec_t_60.value);
+    static_assert(frame_to_tick(one_sec_f, _120bpm).value == one_sec_t_120.value);
 
     return 0;
 }
